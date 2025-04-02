54cd47d9c0d1af5e9809e92fc203568b28b96d51
leekanghoo <protection0828@gmail.com>
2025-04-02 17:41:46 +0900

    first

---
+++ widget.cpp
@@ -0,0 +1,59 @@
+#include "widget.h"
+#include "ui_widget.h"
+
+#include <QMessageBox>
+
+
+Widget::Widget(QWidget *parent)
+    : QWidget(parent)
+    , ui(new Ui::Widget)
+{
+    ui->setupUi(this);
+}
+
+Widget::~Widget()
+{
+    delete ui;
+}
+void Widget::changeMoney(int n){
+    money += n;
+    ui->lcdNumber->display(money);
+
+}
+
+void Widget::on_pb10_clicked()
+{
+    changeMoney(10);
+}
+
+
+void Widget::on_pb50_clicked()
+{
+    changeMoney(50);
+}
+
+
+void Widget::on_pb100_clicked()
+{
+    changeMoney(100);
+}
+
+
+void Widget::on_pb500_clicked()
+{
+    changeMoney(500);
+}
+
+
+void Widget::on_pbCoffee_clicked()
+{
+    changeMoney(-100);
+}
+
+
+void Widget::on_pbReset_clicked()
+{
+    QMessageBox mb;
+    mb.information(nullptr,"title","aaa");
+}
+
